17b7eee2f1263187642acbf9ce2b5c3899da6a9a
hanseungheon <hhanheon@gmail.com>
2022-12-06 01:24:02 +0900

    second commit

--- termproect.py
+++ termproect.py
@@ -15,6 +15,52 @@ if p == 1:
     cursor.execute(sql)
     pr = cursor.fetchall()
     print(pr)
-    
+elif p == 0:
+    k = int(input('원하는 행동의 숫자를 입력하세요. 1.조회, 2.추가, 3.수정, 4.삭제, 5.조건조회 :'))
+# while(True):
+    if k == 1: #조회
+        entity = input('조회하려는 항목을 선택하세요 gym, member, trainer, program, inbody, manage : ')
+        if entity == 'gym':
+            Gym = int(input('지점번호를 입력하세요 :'))
+            sql = f"SELECT * FROM Gym WHERE Gym_id = {Gym}"
+            cursor.execute(sql)
+            pr = cursor.fetchall()
+            print(pr)
+
+        elif entity == 'member':
+            mem_id = int(input('회원번호(휴대전화 뒤자리)를 입력하세요 :'))
+            sql = f"SELECT * FROM Member WHERE mem_id = {mem_id}"
+            cursor.execute(sql)
+            pr = cursor.fetchall()
+            print(pr)
+
+        elif entity == 'trainer':
+            trainer_id = int(input('트레이너 아이디를 입력하세요 :'))
+            sql = f"SELECT * FROM Trainer WHERE trainer_id = {trainer_id}"
+            cursor.execute(sql)
+            pr = cursor.fetchall()
+            print(pr)
+        
+        elif entity == 'program':
+            pid = int(input('프로그램 번호를 입력하세요 :'))
+            sql = f"SELECT * FROM Program WHERE pid = {pid}"
+            cursor.execute(sql)
+            pr = cursor.fetchall()
+            print(pr)
+        
+        elif entity == 'inbody':
+            mem_id = int(input('회원번호(휴대전화 뒤자리)를 입력하세요 :'))
+            sql = f"SELECT * FROM InBody WHERE mem_id = {mem_id}"
+            cursor.execute(sql)
+            pr = cursor.fetchall()
+            print(pr)
+
+        elif entity == 'manage':
+            trainer_id = int(input('트레이너 아이디를 입력하세요 :'))
+            sql = f"SELECT * FROM Manage WHERE trainer_id = {trainer_id}"
+            cursor.execute(sql)
+            pr = cursor.fetchall()
+            print(pr)
+        # break
 conn.commit()
 conn.close()
