832d773b0042e5247370c71e78d2ecf673be76c0
hanseungheon <hhanheon@gmail.com>
2022-12-06 01:28:43 +0900

    third commit

--- termproect.py
+++ termproect.py
@@ -62,5 +62,29 @@ elif p == 0:
             pr = cursor.fetchall()
             print(pr)
         # break
+    if k == 2: #데이터 삽입
+        entity = input('삽입하려는 테이블을 선택하세요 gym, member, trainer, program, inbody, manage(회원 프로그램 등록) : ')
+        if entity == 'gym':
+            Gym_id, Gym_address, Gym_num, Gym_capa = input('지점번호, 지점 주소, 전화번호, 수용인원을 입력하세요 :').split(',')
+            # Gym_id = int(Gym_id)
+            # Gym_capa = int(Gym_capa)
+            sql = f"INSERT INTO Gym (Gym_id, Gym_address, Gym_num, Gym_capa) VALUES ('{Gym_id}', '{Gym_address}', '{Gym_num}', '{Gym_capa}');"
+            cursor.execute(sql)
+        if entity == 'member':
+            mem_id, mname, start_date, end_date, Trainer_id, Gym_id  = input('휴대번호 뒷자리, 이름, 시작일, 종료일, 트레이너번호, 지점번호을 입력하세요 :').split(',')
+            sql = f"INSERT INTO Member (mem_id, mname, start_date, end_date, Trainer_id, Gym_id) VALUES ('{mem_id}', '{mname}', '{start_date}', '{end_date}', '{Trainer_id}' '{Gym_id}');"
+            cursor.execute(sql)
+        if entity == 'trainer':
+            trainer_id, name, number, salary, Gym_id  = input('휴대번호 뒷자리, 이름, 전화번호, 급여, 지점번호을 입력하세요 :').split(',')
+            sql = f"INSERT INTO Trainer (trainer_id, name, number, salary, Gym_id) VALUES ('{trainer_id}', '{name}', '{number}', '{salary}' '{Gym_id}');"
+            cursor.execute(sql)
+        if entity == 'inbody':
+            mem_id, measure_date, age, weight, height, SSM, score  = input('휴대번호 뒷자리, 이름, 전화번호, 급여, 지점번호을 입력하세요 :').split(',')
+            sql = f"INSERT INTO InBody (mem_id, measure_date, age, weight, height, SSM, score) VALUES ('{mem_id}', '{measure_date}', '{age}', '{weight}' '{height}', '{SSM}', '{score}');"
+            cursor.execute(sql)
+        if entity == 'manage':
+            mem_id, pid, trainer_id  = input('회원번호, 프로그램번호, 담당트레이너번호 :').split(',')
+            sql = f"INSERT INTO Manage (mem_id, pid, trainer_id) VALUES ('{mem_id}', '{pid}', '{trainer_id}');"
+            cursor.execute(sql)
 conn.commit()
 conn.close()
